d89a73bc70a0eb0a05510b3f93b2eceeea6dc52c
Daisy-ahnlabio <daisy@ahnlab.io>
2022-09-06 10:29:12 +0900

    22.09.06 로그인 안되는 오류 수정

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
-import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
-
 import authRoutes from "./routes/auth";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
-//  process.env.ORIGIN;
+// const origin = process.env.ORIGIN;
 const origin = "http://localhost:3000";
 app.use(
   cors({
@@ -14,6 +14,7 @@ app.use(
     credentials: true,
   })
 );
+dotenv.config();
 
 app.use(express.json());
 app.use(morgan("dev"));
