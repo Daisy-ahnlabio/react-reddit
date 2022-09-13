979a9435001d4caaa7a942fcd7fa6787f1f4f28b
daisy-ahnlabio <daisy@ahnlab.io>
2022-09-13 18:55:53 +0900

    22.09.13 env 수정

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com",
       "localhost",
-      "ec2-3-36-74-245.ap-northeast-2.compute.amazonaws.com"
+      "ec2-3-36-74-245.ap-northeast-2.compute.amazonaws.com/"
     ]
   }
 }
